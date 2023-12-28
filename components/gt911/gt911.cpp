43055d6caa8808be03c28c456f95ddd1b66e5ade
Robert Kalinowski <rob.kalinowski@gmail.com>
2023-12-29 00:52:56 +0100

    Update gt911.cpp

--- components/gt911/gt911.cpp
+++ components/gt911/gt911.cpp
@@ -1,7 +1,6 @@
 #include "esphome/core/log.h"
 #include "esphome/components/i2c/i2c_bus.h"
 #include "gt911.h"
-#include <Wire.h>
 
 namespace esphome {
 namespace gt911 {
@@ -25,6 +24,14 @@ void GT911::setup(){
   this->store_.pin = this->interrupt_pin_->to_isr();
   this->interrupt_pin_->attach_interrupt(GT911TouchscreenStore::gpio_intr, &this->store_,
                                           gpio::INTERRUPT_FALLING_EDGE);
+
+  // Update display dimensions if they were updated during display setup
+  this->display_width_ = this->display_->get_width();
+  this->display_height_ = this->display_->get_height();
+  this->rotation_ = static_cast<TouchRotation>(this->display_->get_rotation());
+
+  // Trigger initial read to activate the interrupt
+  this->store_.touch = true;                                          
 }
 
 void GT911::loop(){
@@ -48,7 +55,7 @@ void GT911::loop(){
   bool isTouched = touches > 0;
   if (pointInfo & 0x80) {
     if (isTouched) {
-      TouchPoint tp;
+      TouchPoint tp = {0};
       uint8_t data[touches * 8] = {0};
       if (!this->readBlockData(data, 0x8150, touches * 8)) {
         ESP_LOGE(TAG, "Failed to read block data");
@@ -61,27 +68,28 @@ void GT911::loop(){
         uint16_t dimension_one = (buf[3] << 8) | buf[2];
         uint16_t dimension_two = (buf[1] << 8) | buf[0];
 
-        switch (this->rotation_){
+        switch (this->rotation_){  //rotation already done somewhere else
           case ROTATE_0_DEGREES:
-            tp.x = dimension_one;
-            tp.y = this->display_height_ - dimension_two;
-            break;
-          case ROTATE_180_DEGREES:
-            tp.x = this->display_width_ - dimension_one;
-            tp.y = dimension_two;
-            break;
-          case ROTATE_270_DEGREES:
             tp.x = dimension_two;
             tp.y = dimension_one;
             break;
           case ROTATE_90_DEGREES:
-            tp.x = this->display_height_ - dimension_two;
-            tp.y = this->display_width_ - dimension_one;
+            tp.x = dimension_one;
+            tp.y = this->display_height_ - dimension_two;
+            break;
+          case ROTATE_180_DEGREES:
+            tp.x = this->display_width_ - dimension_two;
+            tp.y = this->display_height_ - dimension_one;
+            break;
+          case ROTATE_270_DEGREES:
+            tp.x = this->display_width_ - dimension_one;
+            tp.y = dimension_two;
             break;
           default:
             break;
         }
-        ESP_LOGE(TAG, "TOUCH %i, X: %i Y:%i ONE %i, TWO %i, ROT: %i", i, tp.x, tp.y, dimension_one, dimension_two, this->rotation_);
+
+        ESP_LOGD(TAG, "TOUCH %i, X: %i Y:%i ONE %i, TWO %i, ROT: %i, DW: %i, DH: %i", i, tp.x, tp.y, dimension_one, dimension_two, this->rotation_, this->display_width_, this->display_height_);
 
         this->defer([this, tp]() { this->send_touch_(tp); });
 
