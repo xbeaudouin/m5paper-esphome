32910646335a450ae0cb1d7e87826bd638264056
Xavier Beaudouin <xbeaudouin@gmail.com>
2024-02-12 12:30:29 +0100

    fix

--- components/it8951e/display.py
+++ components/it8951e/display.py
@@ -56,7 +56,7 @@ async def bm8563_read_time_to_code(config, action_id, template_arg, args):
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await display.register_display(var, config)
-    await cg.register_component(var, config)
+    #await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
 
     if CONF_DISPLAY_CS_PIN in config:
